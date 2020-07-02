8453e03f1683a774f14699b03388ded67f73ea32
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-07-02 18:27:24 -0500

    Increase number of particles on command.

--- JSs/motion-setter.js
+++ JSs/motion-setter.js
@@ -1,18 +1,19 @@
 // TODO: figure out how to read the HTML file in Node.js for debugging purposes
 
 var first_dot = compStyle=topValue=i=[]
+const x_initial_position = 400; const y_initial_position = 500;
 var distance = 30; //distance in pixel that the dot travels at each time step
-n_particles = document.getElementById("id-particle-number").value;
+n_particles = Number(document.getElementById("id-particle-number").value);
 var timeoutID = null;
 
 //TODO: create an initial set of objects
-addDot(n_particles)
+addDot(0,n_particles)
 
 
 function setPosition(){
-    distance = document.getElementById("id-particle-distance").value;
+    distance = Number(document.getElementById("id-particle-distance").value);
     n_current = n_particles;
-    n_particles = document.getElementById("id-particle-number").value;
+    n_particles = Number(document.getElementById("id-particle-number").value);
     //INPUT NUMBER OF PARTICLES 
     //IF NUMBER OF PARTICLES ARE DIFFERENT THAN CURRENT NUMBER THEN 
     //(1) IF NUMBER OF NEW PARTICLES IS BIGGER THAN CURRENT NUMBER THEN
@@ -20,7 +21,7 @@ function setPosition(){
     //(2) IF THE NUMBER OF PARTICLES IS SMALLER THAN CURRENT ONES THEN RANDOMLY
     //REMOVE SOME OF THEM BUT DO NOT AFFECT THE REMAINING ONES' TREJECTORY
     if (n_particles>n_current){
-        addDot(n_particles - n_current)
+        addDot(n_current,n_particles - n_current)
     }
     for (i=1;i<=n_particles;i++){
         angle = Math.random()*2*Math.PI;
@@ -40,13 +41,13 @@ function setPosition(){
 /*
 n: number of random particles to creates
 */
-function addDot(n){
-    for (i=1;i<=n;i++){
+function addDot(n_current,n){
+    for (i=n_current+1;i<=(n_current + n);i++){
 
         //TODO: refactor the position setter of a new dot into a function
         angle = Math.random()*2*Math.PI;
-        x_position = 400 + Math.sin(angle)*distance;
-        y_position = 500 + Math.cos(angle)*distance;
+        x_position = x_initial_position + Math.sin(angle)*distance;
+        y_position = y_initial_position + Math.cos(angle)*distance;
         var x = document.createElement('div');
         x.setAttribute("id","dot" + i)
         x.style.width = '25px';
