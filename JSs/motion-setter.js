a2b7668b8d1fb10e0481107db255fceb89d20035
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-07-11 22:31:19 -0500

    two recursives better synchronized

--- JSs/motion-setter.js
+++ JSs/motion-setter.js
@@ -6,9 +6,10 @@ var distance = []; //distance in pixel that the dot travels at each time step
 n_particles = Number(document.getElementById("id-particle-number").value);
 var timeoutID = color=null;
 var purple ='rgb(128,0,128)';var distance = 10;//pixels
-var n_distance_interval = 20;
-var step_delay_time = 100;//move the circles one millisecond at a time
+var n_distance_interval = 10;
+var step_delay_time = 10;//move the circles one millisecond at a time
 var x_end_position = [];y_end_position=[];dx=[];dy=[];end_distance=[];x_end_distance=[];y_end_distance=[];
+
 //TODO: create an initial set of objects
 addDot(0,n_particles)
 
@@ -27,6 +28,7 @@ function setPosition(){
         addDot(n_current,n_particles - n_current)
     }
     //TODO: if n_particles<n_current remove some of the particles without disturbing the others' trajectory
+    end_distance = 0;
     for (i=1;i<=n_particles;i++){
         angle = Math.random()*2*Math.PI;
         dot_i = document.getElementById("dot"+i);
@@ -34,11 +36,10 @@ function setPosition(){
         topValue = compStyle.getPropertyValue("top").replace("px", "");
         leftValue = compStyle.getPropertyValue("left").replace("px","");
 
-        x_end_distance[i] = Number(leftValue) + Math.sin(angle)*distance;
-        y_end_distance[i] = Number(topValue) + Math.cos(angle)*distance;
+        x_end_distance[i] = Number(leftValue) + Math.cos(angle)*distance;
+        y_end_distance[i] = Number(topValue) + Math.sin(angle)*distance;
         dx[i] = Math.floor((x_end_distance[i]-Number(leftValue))/n_distance_interval);
         dy[i] = Math.floor((y_end_distance[i]-Number(topValue))/n_distance_interval);
-        end_distance=0;
     }
 
     miniStep()
@@ -55,10 +56,12 @@ function setPosition(){
                 dot_i.style.left = (Number(leftValue) + dx[i]) + "px";                
             }
             end_distance +=distance/n_distance_interval;
+            console.log(end_distance)
             setTimeout(miniStep,step_delay_time)
+            //keep the execution stuck here...
         }
     }
-    setTimeout(setPosition,0)
+    setTimeout(setPosition,step_delay_time*n_distance_interval*2)
 }
 
 /*
@@ -103,5 +106,7 @@ function randomColor() {
     return 'rgb(' + r1 + "," + r2 + "," + r3 + ')';
 }
 
-
-setPosition();
+setPosition()
+// while (true){
+//     setTimeout(setPosition,0)
+// }
