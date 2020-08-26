ec763f3f46eaf567478dd8fe2064ea8b3260b974
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-08-26 10:19:20 -0500

    some clean-up

--- JSs/main.js
+++ JSs/main.js
@@ -9,9 +9,9 @@ var compStyle=[],
     step_delay_time = 10,//ms
     dx=[],
     dy=[],
-    end_distance=[],
-    x_end_distance=[],
-    y_end_distance=[],
+    traversed_distance=[],
+    x_end_position=[],
+    y_end_position=[],
     angle=0,
     dot_i=[],
     distance = 10,
@@ -44,16 +44,19 @@ scatterPlot()
 function setPosition(){
     scatterPlot(elapsedTime,n_active_particles)
     elapsedTime +=1
-    distance = Number(document.getElementById("id-particle-distance").value);
     step_delay_time = Number(document.getElementById("id-particle-speed").value);
+    distance = Number(document.getElementById("id-particle-distance").value);
+
+    //acquire information about inactive particles
     n_previous = n_particles;
     n_particles = Number(document.getElementById("id-particle-number").value);
 
+    //acquire information about active particles
     n_inactive_previous = n_active_particles;
     n_active_particles = Number(document.getElementById("id-active-particle-number").value);
     activity_strength = Number(document.getElementById("id-activity-strength").value);
 
-    //decide whether to add or remove dots from HTML
+    //decide whether to add or remove inactive from HTML
     if (n_inactive_particles>n_inactive_previous){
         addDot(n_inactive_previous,n_inactive_particles - n_inactive_previous,active=0)
     }
@@ -61,7 +64,7 @@ function setPosition(){
         removeDot(n_inactive_previous,n_inactive_previous-n_inactive_particles,active=0)
     }
 
-    //decide whether to add or remove active dots from HTML
+    //decide whether to add or remove active particles from HTML
     if (n_active_particles>n_active_previous){
         addDot(n_active_previous,n_active_particles - n_active_previous,active=1)
     }
@@ -69,18 +72,17 @@ function setPosition(){
         removeDot(n_active_previous,n_active_previous-n_active_particles,active=1)
     }
 
-
-    dx=[],dy=[],x_end_distance=[],y_end_distance=[]
-    end_distance = 0;
+    //set dx,dy,x_end_position, y_end_position to null arrays
+    dx=[],dy=[],x_end_position=[],y_end_position=[]
+    travesered_distance = 0; //distance particles have traveled at each mini-time step
     for (i=1;i<=(n_active_particles+n_inactive_particles);i++){
-        angle = Math.random()*2*Math.PI;
-        x_distance =  Math.cos(angle)*distance
-        y_distance = Math.sin(angle)*distance
+        angle = Math.random()*2*Math.PI; //select a random angle for the particle to head to
+        x_distance =  Math.cos(angle)*distance //determine the x-coordinate distance of that direction
+        y_distance = Math.sin(angle)*distance //determine the y-coordinate distancee of that direction
 
+        //this section is to avoid directional bias for the random walkers
         rand_ceil_or_floor_x = -0.5 + Math.random()
         rand_ceil_or_floor_y = -0.5 + Math.random()
-
-        //this is to avoid directional bias for the random walkers
         if (rand_ceil_or_floor_x<0){
             dx[i] = Math.floor(x_distance/n_distance_interval)
         }else{
@@ -99,7 +101,7 @@ function setPosition(){
     miniStep()
 
     function miniStep(){
-        if (end_distance<=distance){
+        if (traversed_distance<=distance){
             for (i=1;i<=n_particles;i++){ 
                 //let's put the condition for iteractivity here. If two dots are at close proximity to each other at any single
                 //point, then based on activity-strenght they interact.            
@@ -111,36 +113,36 @@ function setPosition(){
                 within_boundary = true
                 if ((Number(leftValue) + dx[i])>x_bounding_right){
                     within_boundary = false
-                    x_end_distance[i] = x_bounding_right - Math.abs(Number(leftValue)+dx[i]-x_bounding_right);
+                    x_end_position[i] = x_bounding_right - Math.abs(Number(leftValue)+dx[i]-x_bounding_right);
                     dx[i] = -dx[i]
 
                 } else if ((Number(leftValue) + dx[i])<x_bounding_left){
                     within_boundary = false
-                    x_end_distance[i] = x_bounding_left + Math.abs(Number(leftValue)+dx[i]-x_bounding_left);
+                    x_end_position[i] = x_bounding_left + Math.abs(Number(leftValue)+dx[i]-x_bounding_left);
                     dx[i] = -dx[i]
                 }
 
                 if ((Number(topValue) + dy[i])<y_bounding_top){
                     within_boundary = false
-                    y_end_distance[i] = y_bounding_top +  Math.abs(Number(topValue)+dy[i]-y_bounding_top);
+                    y_end_position[i] = y_bounding_top +  Math.abs(Number(topValue)+dy[i]-y_bounding_top);
                     dy[i]=-dy[i]
 
                 } else if ((Number(topValue) + dy[i])>y_bounding_bottom){
                     within_boundary = false
-                    y_end_distance[i] = y_bounding_bottom - Math.abs(Number(topValue)+dy[i]-y_bounding_bottom);
+                    y_end_position[i] = y_bounding_bottom - Math.abs(Number(topValue)+dy[i]-y_bounding_bottom);
                     dy[i]=-dy[i]
                 }
 
                 if (within_boundary){
-                    x_end_distance[i] = Number(leftValue) + dx[i];
-                    y_end_distance[i] = Number(topValue) + dy[i];
+                    x_end_position[i] = Number(leftValue) + dx[i];
+                    y_end_position[i] = Number(topValue) + dy[i];
                 }
 
-                dot_i.style.top = y_end_distance[i] + "px";
-                dot_i.style.left = x_end_distance[i] + "px"; 
+                dot_i.style.top = y_end_position[i] + "px";
+                dot_i.style.left = x_end_position[i] + "px"; 
                 //if condition then flag that circle as active (red)                 
             }
-            end_distance +=distance/n_distance_interval;
+            traversed_distance +=distance/n_distance_interval;
             setTimeout(miniStep,step_delay_time)
         }
     }
